4aa986279c0bcb53c52bd5efae4a3bf4e7a2e49b
rolando167 <rolando167@hotmail.com>
2020-05-26 16:28:34 -0500

    Updates

---
+++ precache-manifest.25dff73bde7dce043d6b26f2f4051034.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5037d6cdf85b1f872c55eecc46a6d46c",
+    "url": "/reactPokemon/index.html"
+  },
+  {
+    "revision": "cf03211e7e4034e0d478",
+    "url": "/reactPokemon/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f9cfe10b88c3b02c1c4",
+    "url": "/reactPokemon/static/js/2.eecc4e48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPokemon/static/js/2.eecc4e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf03211e7e4034e0d478",
+    "url": "/reactPokemon/static/js/main.e626e6f8.chunk.js"
+  },
+  {
+    "revision": "fe1b23d26eddde917e3b",
+    "url": "/reactPokemon/static/js/runtime-main.aa3021e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactPokemon/static/media/logo.5d5d9eef.svg"
+  }
+]);
